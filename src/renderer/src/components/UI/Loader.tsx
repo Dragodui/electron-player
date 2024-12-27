7ba401eca927b0609cfd308b580b1f6459f2fcef
Dragodui <alghazaliaks123@gmail.com>
2024-12-27 22:46:17 +0100

    Loader added

---
+++ src/renderer/src/components/UI/Loader.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface SpinnerProps {
+  size?: string; 
+  color?: string; 
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ size = 'w-[70px] h-[70px]', color = 'border-blue-500' }) => {
+  return (
+   <div className="flex justify-center items-center h-full w-full">
+     <div
+      className={`inline-block ${size} border-4 border-t-transparent ${color} rounded-full animate-spin`}
+      role="status"
+      aria-label="Loading"
+    ></div>
+   </div>
+  );
+};
+
+export default Spinner;
--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -4,16 +4,20 @@ import Button from '@renderer/components/UI/Button';
 import TrackLayout from '@renderer/components/UI/TrackLayout';
 import { FC, useEffect, useState } from 'react';
 import { ISongData } from '../../../types';
+import Loader from '@renderer/components/UI/Loader';
 
 const Home: FC = (): JSX.Element => {
   const [songs, setSongs] = useState<ISongData[]>([]);
   const [currentSongIndex, setCurrentSongIndex] = useState<number>(-1);
+  const [loading, setLoading] = useState<boolean>(false);
   const api = (window as any).api;
 
   const handleFolderSelect = async () => {
     try {
+      setLoading(true);
       const files = await api.getMusicFiles();
       setSongs(files);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -29,8 +33,10 @@ const Home: FC = (): JSX.Element => {
 
   const loadSongsFromDB = async () => {
     try {
+      setLoading(true);
       const songsFromDB: ISongData[] = await api.getMusicFromDB();
       setSongs(songsFromDB);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -47,29 +53,32 @@ const Home: FC = (): JSX.Element => {
         <h1 className="text-4xl font-bold">All songs</h1>
         <Button onClick={handleFolderSelect}>Update Songs</Button>
       </header>
-
-      <div className="flex flex-col gap-4 items-center justify-center ">
-        {songs.length > 0 ? (
-          songs.map((song, index) => (
-            <TrackLayout
-              onClick={() => {
-                setCurrentSongIndex(index);
-              }}
-              song={song}
-              key={index}
-              addStyles={
-                currentSongIndex !== -1 && song.src === songs[currentSongIndex].src
-                  ? 'bg-blue-500'
-                  : 'bg-gray-800'
-              }
-            />
-          ))
-        ) : (
-          <Button addStyles="mt-[100px]" onClick={handleFolderSelect}>
-            Select Folder
-          </Button>
-        )}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col gap-4 items-center justify-center ">
+          {songs.length > 0 ? (
+            songs.map((song, index) => (
+              <TrackLayout
+                onClick={() => {
+                  setCurrentSongIndex(index);
+                }}
+                song={song}
+                key={index}
+                addStyles={
+                  currentSongIndex !== -1 && song.src === songs[currentSongIndex].src
+                    ? 'bg-blue-500'
+                    : 'bg-gray-800'
+                }
+              />
+            ))
+          ) : (
+            <Button addStyles="mt-[100px]" onClick={handleFolderSelect}>
+              Select Folder
+            </Button>
+          )}
+        </div>
+      )}
       <MusicPlayer
         song={currentSongIndex !== -1 ? songs[currentSongIndex] : null}
         onPrevious={handlePreviousSong}
